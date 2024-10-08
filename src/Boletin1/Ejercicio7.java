109db4ea52175da4739e70d43233c615fb09e26b
alba.duque <alba.duque@106-09.info2>
2024-10-08 14:05:45 +0200

    7

--- src/Boletin1/Ejercicio7.java
+++ src/Boletin1/Ejercicio7.java
@@ -10,7 +10,32 @@ public class Ejercicio7 {
 		
 		Scanner sc= new Scanner(System.in);
 		
-		System.out.println(	);
+		
+		System.out.println(	"Introduzca un numero entre el 0 y 99999: ");
+		
+		n= sc.nextInt();
+		
+		if (n<10) {
+			
+			System.out.println("Tiene solo una cifra");
+		}else if (n<100) {
+			
+			System.out.println("Tiene dos cifra");
+		}else if (n<1000) {
+			
+			System.out.println("Tiene tres cifra");
+		}else if (n<10000) {
+			
+			System.out.println("Tiene cuatro cifra");
+		}else if (n<100000) {
+			
+			System.out.println("Tiene cinco cifra");
+		}else if (n>=100000) {
+			
+			System.out.println("Se pide un numero entre 0 y 99999, vuelva a introducir el numero");
+		}
+			
+		
 
 	}
 

765b7d09d1a848c9cb530500f821c0ace5dd0712
albad <albad@192.168.1.33>
2024-10-08 18:12:22 +0200

    8

--- src/Boletin1/Ejercicio8.java
+++ src/Boletin1/Ejercicio8.java
@@ -6,7 +6,7 @@ public class Ejercicio8 {
 
 	public static void main(String[] args) {
 		// Variables
-				int n;
+				double n;
 				
 				//Crea el scanner
 				Scanner sc= new Scanner(System.in);
@@ -15,14 +15,35 @@ public class Ejercicio8 {
 				System.out.println(	"Introduzca tu nota (0-10): ");
 				
 				//escanea el numero intoducido
-				n= sc.nextInt();
+				n= sc.nextDouble();
 				
+				//Dependiendo de la nota ingresada te saldra el resultado correspondiente
 				if(n<5) {
 					
-					System.out.println();
-				}
+					System.out.println("INSUFICIENTE");
+					
+				}else if (n<6) {
+					
+					System.out.println("SUFICIENTE");
+					
+				}else if (n<7) {
+					
+					System.out.println("BIEN");
+					
+				}else if (n<9) {
+					
+					System.out.println("NOTABLE");
+					
+				}else if (n<=10) {
+					
+					System.out.println("SOBRESALIENTE");
+					
+				}else if (n>10) {
+					
+					System.out.println("Vuelve a introducir su nota correspondiente (0-10)");
+				}  
 				
-				
+				sc.close();
 
 	}
 

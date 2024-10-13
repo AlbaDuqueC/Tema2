cf5827404626391484b3e8e42f2f201d742ee86e
albad <albad@192.168.1.33>
2024-10-13 16:57:05 +0200

    6

---
+++ src/boletin3/Ejercicio6.java
@@ -0,0 +1,60 @@
+package boletin3;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+
+	public static void main(String[] args) {
+		//Valores
+		int h;
+		int min;
+		int seg;
+		int seg1;
+		int min1;
+		int h1;
+		
+		//Crea el scanner
+		Scanner sc= new Scanner(System.in);
+		
+		//Pide que introduzcas las horas
+		System.out.println(	"Introduzca la hora: ");
+		
+		//escanea el numero introducida
+		h= sc.nextInt();
+		
+		//Pide que introduzcas los minutos
+		System.out.println(	"Introduzca los minutos: ");
+		
+		//escanea el numero introducida
+		min= sc.nextInt();
+
+		//Pide que introduzcas los segundos
+		System.out.println(	"Introduzca los segundos: ");
+		
+		//escanea el numero introducida
+		seg= sc.nextInt();
+		
+		seg1= seg + 1;
+		min1=min + 1;
+		h1= h + 1;
+		
+		//calcula la hora
+		if (seg1>=60) {
+			
+			if (min1>= 60) {
+				
+				System.out.println(h1 + ":"+ (min1-60) +":"+ (min1-60));
+			}else if (min<60) {
+				System.out.println(h +":"+ min1 + ":" + (seg1-60));
+			}
+		}else if (seg1<60) {
+			System.out.println(h +":"+ min +":"+ seg1);
+		}
+		
+		//cierre del scanner
+		sc.close();
+
+
+	}
+
+}

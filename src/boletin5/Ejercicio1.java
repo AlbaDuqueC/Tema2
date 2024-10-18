b8b8eacbc5bece0b385aaec85b3b581129cd2020
AlbaDuqueC <alba.duque@iesnervion.es>
2024-10-18 13:56:57 +0200

    1

---
+++ src/boletin5/Ejercicio1.java
@@ -0,0 +1,16 @@
+package boletin5;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		//Variables
+		
+		int n=1;
+		
+		do {
+			System.out.println(n++);
+		}while(n<=20);
+
+	}
+
+}

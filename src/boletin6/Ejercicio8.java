5c3b382a2602b664d7a71762e600435675796202
albad <albad@192.168.1.33>
2024-10-23 18:36:38 +0200

    ej 8

--- src/boletin6/Ejercicio8.java
+++ src/boletin6/Ejercicio8.java
@@ -5,18 +5,47 @@ import java.util.Scanner;
 public class Ejercicio8 {
 
 	public static void main(String[] args) {
-		//Variables
+		// Variables
+
+		// Variable que sirve para introducir el valor de numA
 		int numA;
+
+		// Variable que sirve para introducir el valor de numB
 		int numB;
-		
-		Scanner sc= new Scanner(System.in);
-		
-		//
+
+		// Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// Pide el numA
 		System.out.println("Introduzca un numero");
-		numA= sc.nextInt();
-		
+
+		// Escanea el numA
+		numA = sc.nextInt();
+
+		// Pide el numB
 		System.out.println("Introduzca un numero");
-		numB= sc.nextInt();
+
+		// Escanea el numB
+		numB = sc.nextInt();
+
+		// Si numA es menor que numB
+		if (numA < numB) {
+			for (int i = numA; i <= numB; i++) {
+				System.out.println(i);
+			}
+		}
+		// Si numB es menor que numA
+		else if (numB < numA) {
+			for (int i = numB; i <= numA; i++) {
+				System.out.println(i);
+			}
+		}
+		// Si numb y numA son iguales
+		else
+			System.out.println("Es igual");
+
+		// Cierre del scanner
+		sc.close();
 
 	}
 

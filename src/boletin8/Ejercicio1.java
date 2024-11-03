fc8615402a817692f7fdd2109e80f9bc85124156
albad <albad@192.168.1.33>
2024-11-03 13:29:31 +0100

    ej 1

--- src/boletin8/Ejercicio1.java
+++ src/boletin8/Ejercicio1.java
@@ -1,5 +1,6 @@
 package boletin8;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio1 {
@@ -7,16 +8,40 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// Variables
 		// Contiene el valor introducido en el scanner
-		int lado;
+		int lado=0;
+		boolean error= true;
 
 		// Abrir scanner
 		Scanner sc = new Scanner(System.in);
-
-		// Pide por pantalla la dimension del lado
+		
+		
+		do {
+		try {
+			// Pide por pantalla la dimension del lado
 		System.out.println("Introduce las dimensiones del lado");
 
 		// Escanea el valor introducido
 		lado = sc.nextInt();
+		error= true;
+		assert lado >=0 : "Tiene que ser un numero positivo";
+		}catch (InputMismatchException e) {
+			System.err.println("No pongas letras");
+			sc.nextLine();
+			error=false;
+			
+		}catch (AssertionError e) {
+			System.err.println(e);
+			error=false;
+		}
+		
+		finally {
+			System.out.println("");
+			
+		}
+		}while(error==false);
+		
+		
+		
 
 		// Crea el cuadrado con las dimensiones introducidas
 		for (int i = 1; i <= lado; i++) {
